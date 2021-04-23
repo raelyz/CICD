9722788d661232c425789bb2365b1c5df1bb000a
raelyz <73516102+raelyz@users.noreply.github.com>
2021-04-23 14:44:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,12 @@
 package main
 
-import (
-	"./pkg/allnodes"
-	"./pkg/userinterface"
-	"sync"
-)
 
-var (
-	itinerary = allnodes.NewItinerary()
-	login     = userinterface.NewLoginMenu()
-	admin     = userinterface.NewAdminMenu()
-	user      = userinterface.NewUserMenu()
-	wg        sync.WaitGroup
-)
+
+
 
 func main() {
-	userinterface.LoadData()
-	login.SeedData()
-	admin.SeedData()
-	user.SeedData()
-	input := login.MenuOptions()
+
+
 
 	// Possible Location to run goroutines, would require passing the address of the waitgroup through but didn't have time to test for concurrency stability
 	// wg.Add(2)
@@ -27,10 +14,6 @@ func main() {
 	// go user.MenuOptions()
 	// wg.Wait()
 	//
-	if input == 1 {
-		user.MenuOptions()
-	} else if input == 2 {
-		admin.MenuOptions()
-	}
+
 
 }
