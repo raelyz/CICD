cf7ff73a8ccffbf7e6661f2b34f61c8e93373f3c
raelyz <73516102+raelyz@users.noreply.github.com>
2021-04-23 16:44:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,16 +6,6 @@ package main
 
 func main() {
 
-
-
-	// Possible Location to run goroutines, would require passing the address of the waitgroup through but didn't have time to test for concurrency stability
-	// wg.Add(2)
-	// go user.MenuOptions()
-	// go user.MenuOptions()
-	// wg.Wait()
-	//
-
-
 }
 
 func Sum(a,b int) int{
