f5116adf03a6aeed917005ddac747ce189c52fd4
raelyz <73516102+raelyz@users.noreply.github.com>
2021-04-23 16:48:20 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -7,5 +7,8 @@ func TestMain(t *testing.T){
 }
 
 func TestSum(t *testing.T){
-
+  total := Sum(5, 5)
+    if total != 10 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+    }
 }
